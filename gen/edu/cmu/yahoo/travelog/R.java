0749adbec73c6f3084a9ddbf08f5407cdafa7717
Ramya B <ramya201@gmail.com>
2013-09-28 23:47:59 -0700

    Geo coding done and places autocomplete using google api works properly

--- gen/edu/cmu/yahoo/travelog/R.java
+++ gen/edu/cmu/yahoo/travelog/R.java
@@ -30,7 +30,8 @@ public final class R {
         public static final int searchTextView=0x7f080001;
     }
     public static final class layout {
-        public static final int search=0x7f030000;
+        public static final int list_item=0x7f030000;
+        public static final int search=0x7f030001;
     }
     public static final class menu {
         public static final int search=0x7f070000;
--- src/edu/cmu/yahoo/travelog/Search.java
+++ src/edu/cmu/yahoo/travelog/Search.java
@@ -1,75 +1,154 @@
 package edu.cmu.yahoo.travelog;
 
 import java.io.IOException;
-import java.io.StringReader;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.List;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
-
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.app.Activity;
-import android.text.Editable;
-import android.text.TextWatcher;
+import android.content.Context;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.TextView;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Toast;
 
-public class Search extends Activity {
+
+public class Search extends Activity implements OnItemClickListener {
 	private AutoCompleteTextView searchPlace;
-	private ArrayList<String> places = new ArrayList<String>();
-	private final String YAHOO_APP_ID = "oZKjTRfV34HrTKHcm21ofiEWRiTSuyRiPYJ7iGoNUiULPIJtdkIUtEQ_S8xf3B.j4U7RKk1imVenH5XC26hrNb4hQQMc1jE-";
+	private static final String LOG_TAG = "PlaceFinder";
+
+	private static final String PLACES_API_BASE = "https://maps.googleapis.com/maps/api/place";
+	private static final String TYPE_AUTOCOMPLETE = "/autocomplete";
+	private static final String OUT_JSON = "/json";
+
+	private static final String API_KEY = "AIzaSyBmmUHiCEFeJGb6jh3syHcmVigHjHbiuwo";
+	private Context context;
+
+	private ArrayList<String> autocomplete(String input) {
+		ArrayList<String> resultList = null;
+
+		HttpURLConnection conn = null;
+		StringBuilder jsonResults = new StringBuilder();
+		try {
+			StringBuilder sb = new StringBuilder(PLACES_API_BASE + TYPE_AUTOCOMPLETE + OUT_JSON);
+			sb.append("?sensor=false&key=" + API_KEY);
+			sb.append("&components=country:us");
+			sb.append("&input=" + URLEncoder.encode(input, "utf8"));
+
+			URL url = new URL(sb.toString());
+			conn = (HttpURLConnection) url.openConnection();
+			InputStreamReader in = new InputStreamReader(conn.getInputStream());
+
+			// Load the results into a StringBuilder
+			int read;
+			char[] buff = new char[1024];
+			while ((read = in.read(buff)) != -1) {
+				jsonResults.append(buff, 0, read);
+			}
+		} catch (MalformedURLException e) {
+			Log.e(LOG_TAG, "Error processing Places API URL", e);
+			return resultList;
+		} catch (IOException e) {
+			Log.e(LOG_TAG, "Error connecting to Places API", e);
+			return resultList;
+		} finally {
+			if (conn != null) {
+				conn.disconnect();
+			}
+		}
+
+		try {
+			// Create a JSON object hierarchy from the results
+			JSONObject jsonObj = new JSONObject(jsonResults.toString());
+			JSONArray predsJsonArray = jsonObj.getJSONArray("predictions");
+
+			// Extract the Place descriptions from the results
+			resultList = new ArrayList<String>(predsJsonArray.length());
+			for (int i = 0; i < predsJsonArray.length(); i++) {
+				resultList.add(predsJsonArray.getJSONObject(i).getString("description"));
+			}
+		} catch (JSONException e) {
+			Log.e(LOG_TAG, "Cannot process JSON results", e);
+		}
+
+		return resultList;
+	}
+	private class PlacesAutoCompleteAdapter extends ArrayAdapter<String> implements Filterable {
+		private ArrayList<String> resultList;
+
+		public PlacesAutoCompleteAdapter(Context context, int textViewResourceId) {
+			super(context, textViewResourceId);
+		}
+
+		@Override
+		public int getCount() {
+			return resultList.size();
+		}
+
+		@Override
+		public String getItem(int index) {
+			return resultList.get(index);
+		}
+
+		@Override
+		public Filter getFilter() {
+			Filter filter = new Filter() {
+				@Override
+				protected FilterResults performFiltering(CharSequence constraint) {
+					FilterResults filterResults = new FilterResults();
+					if (constraint != null) {
+						// Retrieve the autocomplete results.
+						resultList = autocomplete(constraint.toString());
+
+						// Assign the data to the FilterResults
+						filterResults.values = resultList;
+						filterResults.count = resultList.size();
+					}
+					return filterResults;
+				}
+
+				@Override
+				protected void publishResults(CharSequence constraint, FilterResults results) {
+					if (results != null && results.count > 0) {
+						notifyDataSetChanged();
+					}
+					else {
+						notifyDataSetInvalidated();
+					}
+				}};
+				return filter;
+		}
+	}
 
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.search);
+		
+		context = getApplicationContext();
 
 		searchPlace = (AutoCompleteTextView) findViewById(R.id.searchTextView);
+		searchPlace.setAdapter(new PlacesAutoCompleteAdapter(this, R.layout.list_item));
 
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_dropdown_item_1line, places);
-		searchPlace.setAdapter(adapter);
-
-		searchPlace.addTextChangedListener(new TextWatcher() {
-			@Override
-			public void afterTextChanged(Editable arg0) {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public void beforeTextChanged(CharSequence s, int start, int count,
-					int after) {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public void onTextChanged(CharSequence s, int start, int before,
-					int count) {
-				//getPlaces(searchPlace.getText().toString());			
-			}
-		});        
+		searchPlace.setOnItemClickListener(this);	
 	}
 
 	@Override
@@ -79,58 +158,41 @@ public class Search extends Activity {
 		return true;
 	}
 
-	public void findPhotos(View view)
-	{
-		getPlaces(searchPlace.getText().toString());
-	}
+	@Override
+	public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+		final String str = (String) adapterView.getItemAtPosition(position);
 
-	public void getPlaces(final String startLetters)
-	{
-		Thread placesThread = new Thread(new Runnable()
+		Thread geocoderThread = new Thread(new Runnable()
 		{
 
 			@Override
 			public void run() {
+				Geocoder gc = new Geocoder(context);
 
-				String requestText = "http://where.yahooapis.com/v1/places.q(" + startLetters + ")?appid=" + YAHOO_APP_ID;
-				HttpResponse response = null;
-				try {        
-					HttpClient client = new DefaultHttpClient();
-					HttpGet request = new HttpGet(requestText);
-					request.setHeader("Accept", "application/xml");
-					response = client.execute(request);
+				if(Geocoder.isPresent()){
+					List<Address> list;
+					try {
+						list = gc.getFromLocationName(str, 1);
+						Address address = list.get(0);
 
-					HttpEntity entity = response.getEntity();
-					String xmlString = EntityUtils.toString(entity);
-					//Log.d("XML String",xmlString);
-
-					DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-					DocumentBuilder db = factory.newDocumentBuilder();
-					InputSource inStream = new InputSource();
-					inStream.setCharacterStream(new StringReader(xmlString));
-					Document doc = db.parse(inStream); 
-
-					NodeList list = doc.getElementsByTagName("place");
-					for (int i =0; i< list.getLength(); i++)
-					{
-						places.add(list.item(i).getChildNodes().item(2).getNodeValue());
-						Log.d("Place Name",list.item(i).getChildNodes().item(2).getNodeValue());
-					}
-
-				} catch (ClientProtocolException e) {
-					e.printStackTrace();
-				} catch (IOException e) {
-					e.printStackTrace();
-				} catch (ParserConfigurationException e) {
-					e.printStackTrace();
-				} catch (SAXException e) {
-					e.printStackTrace();
-				} 
-
+						double lat = address.getLatitude();
+						double lng = address.getLongitude();
+						
+						Log.d(LOG_TAG,"Latitude:"+lat);
+						Log.d(LOG_TAG,"Longitude:"+lng);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}									
+				}
 			}
-
 		});
-		placesThread.start();     
+		geocoderThread.start();
 	}
-}
+	
+	public void findPhotos(View view)
+	{
+		
+	}
+}	
+
 
